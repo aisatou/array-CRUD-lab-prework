ff3fc9b9856f6607f94b29f6f0de16fe2cc6b330
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-07 00:12:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,11 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   element = array.pop
-  return element
+  return array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  element = array.shift
+  return array
 end
 
 def retrieve_element_from_index(array, index_number)
